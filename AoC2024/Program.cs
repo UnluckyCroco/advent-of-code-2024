ce5fe9f87bb8defc5df138ab8b902b25ca76ea69
Daniël Phoeng <danielphoeng@gmail.com>
2024-12-01 23:44:48 +0100

    init

---
+++ AoC2024/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace AoC2024
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Ex1A());
+            Console.WriteLine(Ex1B());
+        }
+
+        private static int Ex1A()
+        {
+            var lines = File.ReadAllLines("./input/1.txt").ToList();
+
+            var firstList = new List<int>();
+            var secondList = new List<int>();
+            lines.Select(line => line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)).ToList()
+                .ForEach(ball =>
+                {
+                    firstList = firstList.Append(Convert.ToInt32(ball[0])).ToList();
+                    secondList = secondList.Append(Convert.ToInt32(ball[1])).ToList();
+                });
+            
+            firstList.Sort();
+            secondList.Sort();
+
+            return firstList.Select((t, i) => Math.Abs(t - secondList[i])).Sum();
+        }
+        
+        private static int Ex1B()
+        {
+            var lines = File.ReadAllLines("./input/1.txt").ToList();
+
+            var firstList = new List<int>();
+            var secondList = new List<int>();
+            lines.Select(line => line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)).ToList()
+                .ForEach(ball =>
+                {
+                    firstList = firstList.Append(Convert.ToInt32(ball[0])).ToList();
+                    secondList = secondList.Append(Convert.ToInt32(ball[1])).ToList();
+                });
+            
+            return firstList.Select(t => secondList.FindAll(e => e == t).Count * t).Sum();
+        }
+    }
+}
