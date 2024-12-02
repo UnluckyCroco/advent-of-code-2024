776eec34a44bd6d6d8b9b6c9d538511f54b4431f
Daniël Phoeng <danielphoeng@gmail.com>
2024-12-02 17:38:13 +0100

    Day 2

--- AoC2024/Program.cs
+++ AoC2024/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -10,8 +9,106 @@ namespace AoC2024
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(Ex1A());
-            Console.WriteLine(Ex1B());
+            // Console.WriteLine(Ex1A());
+            // Console.WriteLine(Ex1B());
+            // Console.WriteLine(Ex2A());
+            Console.WriteLine(Ex2B());
+        }
+
+        private static void PrintList(List<int> list, string separator = " ", string end = "\n")
+        {
+            foreach (var i in list)
+            {
+                Console.Write(i + separator);
+            }
+            Console.Write(end);
+        }
+        
+        private static int Ex2A()
+        {
+            var lines = File.ReadAllLines("./input/2.txt").ToList();
+
+            var newLines = lines.Select(line => line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+
+            var counter = 0;
+            foreach (var line in newLines)
+            {
+                var isAsc = line[0] < line[line.Count - 1] ? -1 : 1;
+                counter++;
+                for (var i = 0; i < line.Count - 1; i++)
+                {
+                    if ((line[i] - line[i + 1]) * isAsc > 3 || (line[i] - line[i + 1]) * isAsc < 1)
+                    {
+                        counter--;
+                        break;
+                    }
+
+                    if ((isAsc != 1 || line[i] >= line[i + 1]) && (isAsc != -1 || line[i] <= line[i + 1])) continue;
+                    counter--;
+                    break;
+                }
+            }
+            
+            return counter;
+            // return lines.Select(line => line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                // .Aggregate((x, y) => Math.Abs(x - y) <= 3 && Math.Abs(x - y) >= 1 ? y : -100)).Count(val => val > 0);
+        }
+        
+        private static int Ex2B()
+        {
+            var lines = File.ReadAllLines("./input/2.txt").ToList();
+
+            var newLines = lines.Select(line => line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()).ToList();
+
+            return newLines.Select(Fuck).Count(ret => ret);
+        }
+
+        private static bool Fuck(List<int> line)
+        {
+            var originalLine = new List<int>(line);
+            var isAsc = line[0] < line[line.Count - 1] ? -1 : 1;
+            var hasErrored = -1;
+            var tryOther = false;
+            for (var i = 0; i < line.Count - 1; i++)
+            {
+                if ((line[i] - line[i + 1]) * isAsc > 3 || (line[i] - line[i + 1]) * isAsc < 1)
+                {
+                    if (hasErrored == -1)
+                    {
+                        line.RemoveAt(i);
+                        hasErrored = i;
+                        i = -1;
+                        continue;
+                    }
+
+                    if (tryOther) return false;
+
+                    tryOther = true;
+                    line = originalLine;
+                    line.RemoveAt(hasErrored + 1);
+                    i = -1;
+                    continue;
+                }
+
+                if ((isAsc != 1 || line[i] >= line[i + 1]) && (isAsc != -1 || line[i] <= line[i + 1])) continue;
+                if (hasErrored == -1)
+                {
+                    line.RemoveAt(i);
+                    hasErrored = i;
+                    i = -1;
+                    continue;
+                }
+
+                if (tryOther) return false;
+
+                tryOther = true;
+                line = originalLine;
+                line.RemoveAt(hasErrored + 1);
+                PrintList(line);
+                i = -1;
+            }
+
+            return true;
         }
 
         private static int Ex1A()
@@ -26,13 +123,13 @@ namespace AoC2024
                     firstList = firstList.Append(Convert.ToInt32(ball[0])).ToList();
                     secondList = secondList.Append(Convert.ToInt32(ball[1])).ToList();
                 });
-            
+
             firstList.Sort();
             secondList.Sort();
 
             return firstList.Select((t, i) => Math.Abs(t - secondList[i])).Sum();
         }
-        
+
         private static int Ex1B()
         {
             var lines = File.ReadAllLines("./input/1.txt").ToList();
@@ -45,7 +142,7 @@ namespace AoC2024
                     firstList = firstList.Append(Convert.ToInt32(ball[0])).ToList();
                     secondList = secondList.Append(Convert.ToInt32(ball[1])).ToList();
                 });
-            
+
             return firstList.Select(t => secondList.FindAll(e => e == t).Count * t).Sum();
         }
     }
